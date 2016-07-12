77e84c3a5aac8b2e96b2bfceafaec85e8e5345ad
Electron <devversitymail@gmail.com>
2016-07-13 02:04:10 +0530

    WIP-initial commit
    First commit with UI, preview is broken

---
+++ Readme.md
@@ -0,0 +1,4 @@
+#### WHS Playground
+An client side Playground to play with Whitestorm.js right into your browser [WIP]
+
+inspired and inherited from [CloudEdit](https://github.com/TheInsomniac/CloudEdit)
---
+++ js/cloudEdit.js
@@ -0,0 +1,232 @@
+/*global $:false, ace:false, htmlField:false, cssField:false, jsField:false, jqconsole:false*/
+(function cloudEdit() {
+  "use strict";
+  // Globals
+  // ---
+  // For buildOutput() creation. Toggle includes in html output.
+  var use = {
+    liveEdit: true
+  };
+
+  // End Globals
+
+   // Check if a new appcache is available on page load. If so, ask to load it.
+  window.addEventListener("load", function(e) {
+    window.applicationCache.addEventListener("updateready", function(e) {
+      if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+        // Browser downloaded a new app cache.
+        if (confirm("A new version of this site is available. Load it?")) {
+          window.location.reload();
+        }
+      } else {
+        // Manifest didn't changed. Do NOTHING!
+      }
+    }, false);
+  }, false);
+
+  // Create Text Area panes
+  // Init ACE Editor and set options;
+  (function initAce() {
+    var aceTheme;
+    if (localStorage.getItem("theme")) {
+      aceTheme = localStorage.getItem("theme");
+    } else {
+      aceTheme = "ace/theme/twilight";
+    }
+
+
+    // JS Editor
+    window.jsField = ace.edit("js");
+    jsField.setOptions({
+      theme: aceTheme,
+      displayIndentGuides: true,
+      mode: "ace/mode/javascript",
+      tabSize: 2,
+      useSoftTabs: true,
+      showPrintMargin: false,
+
+    });
+
+    // Retrieve values from sessionStorage if set
+    (function sessionStorageGet() {
+
+
+      if (sessionStorage.getItem("js")) {
+        jsField.setValue(sessionStorage.getItem("js"));
+        jsField.clearSelection();
+      }
+      if (sessionStorage.getItem("use")) {
+        use = JSON.parse(sessionStorage.getItem("use"));
+      }
+
+    })();
+
+  })();
+  // END ACE Editor
+
+  $("#previewToggle, #iframeClose").on("click", function() {
+    $("#previewToggle").toggleClass("btn-active");
+    $("html").toggleClass("modal-open");
+  });
+
+
+  // Used by preview and download to compile editor panes and "Imports" into valid html
+  function buildOutput(consoleJS) {
+
+    var content = {
+      js: jsField.getValue()
+    };
+
+    // String to hold elements to build HTML output
+    // This html is what we show in preview pane
+    var html = '';
+    html += '<!DOCTYPE html>\n';
+    html += '<html lang="en">\n';
+    html += '<head>\n';
+    html += '<meta charset="UTF-8">\n';
+
+    html += '<style type="text/css">\n';
+    html += content.style;
+    html += '\n</style>\n';
+
+    html += '<script src="whitestorm.js"></script>\n';
+
+    html += '</head>\n';
+    html += '<body>\n';
+    html += content.html;
+
+    html += '\n<script>\n';
+    //add boilerplaete here before putting content from users
+    html += content.js;
+    html += '\n</script>\n';
+    html += '</body>\n';
+    html += '</html>';
+
+    //log out preview output
+    console.log(html);
+
+    return html;
+  }
+
+
+  // Update preview window AND js console on click of "Run" button
+  $("#run").on("click", function() {
+    preview();
+  });
+
+  function preview(delay) {
+    delay = delay || 0;
+    var timer = null;
+    if (timer) {
+      window.clearTimeout(timer);
+    }
+    timer = window.setTimeout(function() {
+      timer = null;
+      // pass true as we want the pseudo console.js script
+      //console.time('buildOutput'); // start timer for debugging
+      var textToWrite = buildOutput(true);
+
+      (document.getElementById("iframe").contentWindow.document).write(textToWrite);
+      (document.getElementById("iframe").contentWindow.document).close();
+      //console.timeEnd('buildOutput'); // end timer for debugging
+    }, delay);
+  }
+
+  // Download HTML/CSS/JS
+  // Source: http://thiscouldbebetter.wordpress.com/2012/12/18/loading-editing-and-saving-a-text-file-in-html5-using-javascrip/
+  $("#download").on("click", function() {
+
+    function destroyClickedElement(event) {
+      document.body.removeChild(event.target);
+    }
+
+    var $download = document.createElement("a");
+
+    // pass false as we don't want the pseudo console.js script
+    var textToWrite = buildOutput(false);
+    var textFileAsBlob = new Blob([textToWrite], {type: "text/plain"});
+    var fileNameToSaveAs = "index.html";
+
+    $download.download = fileNameToSaveAs;
+
+    if (typeof window.URL === "function") {
+      // Chrome
+      $download.href = window.URL.createObjectURL(textFileAsBlob);
+    } else {
+      // Firefox
+      $download.href = window.URL.createObjectURL(textFileAsBlob);
+    }
+    $download.onclick = destroyClickedElement;
+		$download.style.display = "none";
+		document.body.appendChild($download);
+    $download.click();
+  });
+
+  // Clear editors with "Clear" button
+  $("#clear").on("click", function() {
+      console.log("clear windows");
+    jsField.setValue("");
+    sessionStorage.clear();
+    (document.getElementById("iframe").contentWindow.document).write("");
+    (document.getElementById("iframe").contentWindow.document).close();
+  });
+
+  // Save current editor panes to localStorage
+  $("#save").on("click", function() {
+    var store = {
+      js: jsField.getValue()
+    };
+    localStorage.setItem("cloudEdit", JSON.stringify(store));
+  });
+
+  // Load into editors from localStorage if exists
+  $("#load").on("click", function() {
+    var store;
+    if (localStorage.cloudEdit) {
+      store = JSON.parse(localStorage.cloudEdit);
+      jsField.setValue(store.js);
+      jsField.clearSelection();
+    } else {
+      alert("No previous session found...");
+    }
+  });
+
+
+  // Apply theme and save to localStorage
+  function updateTheme(theme) {
+    theme = "ace/theme/" + theme;
+    // htmlField.setTheme(theme);
+    // cssField.setTheme(theme);
+    jsField.setTheme(theme);
+    // Uncomment below if you want the page/body background to follow the set theme colour.
+    // we delay obtaining the css colour by 1s as it takes a moment to propagate
+    /*
+    setTimeout(function() {
+      $("body, section").css("background-color", $("#html").css("background-color"));
+    }, 1000);
+    */
+    localStorage.setItem("theme", theme);
+  }
+
+  // Detect a user leaving a page and display a message
+  window.onbeforeunload = function (e) {
+
+    // Save current buffers into sessionStorage
+    // sessionStorage.setItem("html", htmlField.getValue());
+    // sessionStorage.setItem("css", cssField.getValue());
+    sessionStorage.setItem("js", jsField.getValue());
+
+    // save selected imports into sessionStorage
+    sessionStorage.setItem("use", JSON.stringify(use));
+    // // and if using LESS/Sass make sure the editor mode is saved as well
+    // sessionStorage.setItem("cssMode", cssField.getSession().getMode().$id);
+
+    // If we haven't been passed the event get the window.event
+    e = e || window.event;
+    var message = "Your current session may be lost..";
+    // // For IE6-8 and Firefox prior to version 4
+    if (e) e.returnValue = message;
+    // // For Chrome, Safari, IE8+ and Opera 12+
+    return message;
+  };
+})();
